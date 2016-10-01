38ac0679d3a5f24ed0bd317a47325387c7e2a54a
mack1070101 <mackenziebligh@gmail.com>
2016-09-30 21:32:38 -0600

    completed q3; do not save if you read files

--- part3_mb/encrypt.sh
+++ part3_mb/encrypt.sh
@@ -1,4 +1,17 @@
-openssl enc -e -des-ofb -nosalt -in plaintext -out cipherofb.enc
-supersecure
-supersecure
+openssl enc -e -des-ecb -nosalt -in plaintext -out cipherecb.enc -k "01234567" 
 
+openssl enc -e -des-cbc -nosalt -in plaintext -out ciphercbc.enc -k "01234567"
+
+openssl enc -e -des-cfb -nosalt -in plaintext -out ciphercfb.enc -k "01234567"
+
+openssl enc -e -des-ofb -nosalt -in plaintext -out cipherofb.enc -k "01234567"
+
+
+
+du -b cipherecb.enc
+             
+du -b ciphercbc.enc
+             
+du -b ciphercfb.enc
+             
+du -b cipherofb.enc
